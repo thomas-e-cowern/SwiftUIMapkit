6564baa1fa3fe5f5486f82c021fe9bad63a567f8
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-18 11:38:46 -0500

    first commit

---
+++ HelloMaps/HelloMaps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloMaps
+//
+//  Created by Thomas Cowern on 12/18/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HelloMaps/HelloMaps/HelloMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMapsApp.swift
+//  HelloMaps
+//
+//  Created by Thomas Cowern on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HelloMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
