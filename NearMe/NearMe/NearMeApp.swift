4a8ffbe32bfe243834961fe364f0bd5aeab2ae60
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-20 08:32:16 -0500

    near me initial commit

---
+++ NearMe/NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Thomas Cowern on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
