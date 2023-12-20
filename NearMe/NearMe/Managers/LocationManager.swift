8876046c3f9785e001f11a822c94e530ffb9e006
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-20 10:03:30 -0500

    add error enum and descriptions to location manager

--- NearMe/NearMe/Managers/LocationManager.swift
+++ NearMe/NearMe/Managers/LocationManager.swift
@@ -9,6 +9,32 @@ import Foundation
 import MapKit
 import Observation
 
+enum LocationError: LocalizedError {
+    case authorizationDenied
+    case authorizationRestricted
+    case unknownLocation
+    case accessDenied
+    case network
+    case operationFailed
+    
+    var errorDescription: String? {
+        switch self {
+        case .authorizationDenied:
+            return NSLocalizedString("Location access is denied", comment: "")
+        case .authorizationRestricted:
+            return NSLocalizedString("Location access is restricted", comment: "")
+        case .unknownLocation:
+            return NSLocalizedString("This is an unknown location", comment: "")
+        case .accessDenied:
+            return NSLocalizedString("Access is denied", comment: "")
+        case .network:
+            return NSLocalizedString("The network failed", comment: "")
+        case .operationFailed:
+            return NSLocalizedString("The operation had failed", comment: "")
+        }
+    }
+}
+
 @Observable
 class LocationManager: NSObject, CLLocationManagerDelegate {
     
