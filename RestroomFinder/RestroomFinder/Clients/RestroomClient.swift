fc0140078e8456ea3155a0a85a979abb9b9ce3fa
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-23 11:56:01 -0500

    add clients folder and restroom client

---
+++ RestroomFinder/RestroomFinder/Clients/RestroomClient.swift
@@ -0,0 +1,31 @@
+//
+//  RestroomClient.swift
+//  RestroomFinder
+//
+//  Created by Thomas Cowern on 12/23/23.
+//
+
+import Foundation
+
+struct RestroomClient {
+    
+    private enum RestroomClientError: Error {
+        case invalidResponse
+        case networkError(Error)
+    }
+    
+    func fetchRestrooms(url: URL) async throws -> [Restroom] {
+        
+        let (data, response) = try await URLSession.shared.data(from: url)
+        
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw RestroomClientError.invalidResponse
+        }
+        
+        do {
+            return try JSONDecoder().decode([Restroom].self, from: data)
+        } catch {
+            throw RestroomClientError.networkError(error)
+        }
+    }
+}
