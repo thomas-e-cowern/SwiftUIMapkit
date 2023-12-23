f10f8580f367342286595b8904be29988e821091
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-23 10:13:39 -0500

    restroom finder initial commit

---
+++ RestroomFinder/RestroomFinder/RestroomFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestroomFinderApp.swift
+//  RestroomFinder
+//
+//  Created by Thomas Cowern on 12/23/23.
+//
+
+import SwiftUI
+
+@main
+struct RestroomFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
