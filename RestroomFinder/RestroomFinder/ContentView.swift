2d6fe5e0024447d4e3844ba1932a94d72ef43fc1
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-12-26 16:41:17 -0500

    update content view visible region variable

--- RestroomFinder/RestroomFinder/ContentView.swift
+++ RestroomFinder/RestroomFinder/ContentView.swift
@@ -14,10 +14,12 @@ struct ContentView: View {
     @State private var locationManager = LocationManager.shared
     @State private var restrooms: [Restroom] = []
     @State private var selectedRestroom: Restroom?
+    @State private var visibleRegion: MKCoordinateRegion?
+    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
     
     var body: some View {
         ZStack {
-            Map {
+            Map(position: $position) {
                 ForEach(restrooms, id: \.id) { restroom in
                     Annotation(restroom.name, coordinate: restroom.coordinate) {
                         ZStack {
@@ -40,26 +42,40 @@ struct ContentView: View {
                 UserAnnotation()
             }
         }
-        .task(id: locationManager.region) {
+        .task(id: visibleRegion) {
             await loadRestrooms()
+        }
+        .onMapCameraChange({ context in
+            visibleRegion = context.region
+        })
+        .overlay(alignment: .topLeading) {
+            Button {
+                Task {
+                    await loadRestrooms()
+                }
+            } label: {
+                Image(systemName: "arrow.clockwise.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white, .blue)
+            }
+            .padding(.leading, 10)
         }
     }
     
     private func loadRestrooms() async {
         
-        guard let region = locationManager.region else { return }
+        guard let region = visibleRegion else { return }
         let coordinates = region.center
-        
+        print("Coords: \(coordinates)")
         do {
             restrooms = try await restroomClient.fetchRestrooms(url: Constants.Urls.restroomsByLocation(latitude: coordinates.latitude, longitude: coordinates.longitude))
         } catch {
             print(error.localizedDescription)
         }
-        
     }
 }
 
 #Preview {
     ContentView()
-        .environment(\.httpClient, MockRestroomClient())
+        .environment(\.httpClient, RestroomClient())
 }
